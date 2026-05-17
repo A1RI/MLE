42e757208ca5469555955596ca034dfa520a4126
A1RI <louispeklh@gmail.com>
2026-05-17 22:25:46 +0800

    Populated silver layer

---
+++ lab_2/utils/data_processing_silver_table_attributes.py
@@ -0,0 +1,60 @@
+import os
+import glob
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+import pprint
+import pyspark
+import pyspark.sql.functions as F
+import argparse
+
+from pyspark.sql.functions import col
+from pyspark.sql.types import StringType, IntegerType, FloatType, DateType
+
+from pyspark.sql.functions import regexp_extract, when
+from pyspark.ml.feature import VectorAssembler, StandardScaler 
+from pyspark.ml import Pipeline
+from pyspark.ml.feature import StringIndexer, OneHotEncoder
+
+
+def process_silver_table(snapshot_date_str, bronze_directory, feature, silver_directory, spark):
+    # prepare arguments
+    snapshot_date = datetime.strptime(snapshot_date_str, "%Y-%m-%d")
+
+    # connect to bronze table
+    partition_name = "bronze_feature_" + feature + "_" + snapshot_date_str.replace('-','_') + '.csv'
+    filepath = bronze_directory + partition_name
+    df = spark.read.csv(filepath, header=True, inferSchema=True)
+    print('loaded from:', filepath, 'row count:', df.count())
+
+    # clean data: enforce schema / data type
+    # Drop exact duplicate rows
+    df = df.dropDuplicates()
+
+    # Remove rows missing critical fields
+    df = df.dropna()
+
+    # Convert age from string to float
+    df = df.withColumn("age", regexp_extract(col("age"), r"([0-9]+(?:\.[0-9]+)?)", 1).cast("double"))
+
+    # One-hot encode
+    indexer = StringIndexer(inputCol="Occupation", outputCol="occupation_index", handleInvalid="keep")
+
+    encoder = OneHotEncoder(inputCols=["occupation_index"], outputCols=["occupation_ohe"], dropLast=False)
+
+    pipeline = Pipeline(stages=[indexer, encoder])
+    df_encoded = pipeline.fit(df).transform(df)
+    df_encoded.select("occupation", "occupation_index", "occupation_ohe").show(20, truncate=False)
+
+    # save silver table - IRL connect to database to write
+    partition_name = feature + snapshot_date_str.replace('-','_') + '.parquet'
+    filepath = silver_directory + partition_name
+    df.write.mode("overwrite").parquet(filepath)
+    # df.toPandas().to_parquet(filepath,
+    #           compression='gzip')
+    print('saved to:', filepath)
+    
+    return df
